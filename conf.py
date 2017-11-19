f142f5b1432340f49dd5a8f2700dddc5a9003dce
Roger <jayluo9410@gmail.com>
2017-11-18 16:53:31 -0800

    asdas

--- conf.py
+++ conf.py
@@ -1,5 +1,9 @@
 import os
 
+
+AWS = True
+
+
 # file path
 CUR_PATH = os.path.dirname(os.path.abspath(__file__))
 STOPWORD = CUR_PATH + "/data/stopword"
@@ -10,8 +14,12 @@ TECHWORD = CUR_PATH + "/data/techlist"
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = CUR_PATH +"/data/My Project 68452-ae0ad4fc4ab7.json"
 
 # local hadoop path
-STREAM_JAR_PATH = "/Users/roger/usr/local/hadoop-2.8.2/share/hadoop/tools/lib/hadoop-streaming-2.8.2.jar"
-HADOOP_BIN_PATH = "/Users/roger/usr/local/hadoop-2.8.2/bin/hadoop"
+if AWS:
+    STREAM_JAR_PATH ="/usr/lib/hadoop-mapreduce/hadoop-streaming.jar"
+    HADOOP_BIN_PATH = "/usr/bin/hadoop"
+else:
+    STREAM_JAR_PATH = "/Users/roger/usr/local/hadoop-2.8.2/share/hadoop/tools/lib/hadoop-streaming-2.8.2.jar"
+    HADOOP_BIN_PATH = "/Users/roger/usr/local/hadoop-2.8.2/bin/hadoop"
 
 # local io path
 MAPPER_PATH  =    CUR_PATH + "/rake" + "/Mapper.py"
