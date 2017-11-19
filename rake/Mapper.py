e7febe89ad763bab956a18c838711d335ca2df58
Roger <jayluo9410@gmail.com>
2017-11-18 17:16:49 -0800

    AWS!AWS!

--- rake/Mapper.py
+++ rake/Mapper.py
@@ -1,14 +1,383 @@
-#!/usr/bin/env python
+#!/usr/bin/python
 """A Rake Mapper"""
 """output separate key and value by comma!"""
 
+
+from __future__ import absolute_import
+from __future__ import print_function
 import sys
-sys.path.append("..")
-from mod.CxExtractor import CxExtractor as extractor
-import mod.rake as rake
-import operator
 import re
-import conf
+import operator
+import six
+from six.moves import range
+from collections import Counter
+import requests
+
+class CxExtractor:
+    """cx-extractor implemented in Python"""
+
+    __text = []
+    # __threshold = 186
+    __indexDistribution = []
+    # __blocksWidth = 3
+
+    def __init__(self, threshold=86, blocksWidth=3):
+        self.__blocksWidth = blocksWidth
+        self.__threshold = threshold
+
+    def getText(self, content):
+        if self.__text:
+            self.__text = []
+        lines = content.split('\n')
+        for i in range(len(lines)):
+            # lines[i] = lines[i].replace("\\n", "")
+            if lines[i] == ' ' or lines[i] == '\n':
+                lines[i] = ''
+        self.__indexDistribution = []
+        for i in range(0, len(lines) - self.__blocksWidth):
+            wordsNum = 0
+            for j in range(i, i + self.__blocksWidth):
+                lines[j] = lines[j].replace("\\s", "")
+                wordsNum += len(lines[j])
+            self.__indexDistribution.append(wordsNum)
+        start = -1
+        end = -1
+        boolstart = False
+        boolend = False
+        for i in range(len(self.__indexDistribution) - 1):
+            if(self.__indexDistribution[i] > self.__threshold and (not boolstart)):
+                #if (self.__indexDistribution[i + 1] != 0 or self.__indexDistribution[i + 2] != 0 or self.__indexDistribution[i + 3] != 0):
+                if (sum(self.__indexDistribution[i+1:i+4]) != 0):
+                    boolstart = True
+                    start = i
+                    continue
+            if (boolstart):
+                if (self.__indexDistribution[i] == 0 or self.__indexDistribution[i + 1] == 0):
+                    end = i
+                    boolend = True
+            tmp = []
+            if(boolend):
+                for ii in range(start, end + 1):
+                    if(len(lines[ii]) < 5):
+                        continue
+                    tmp.append(lines[ii] + "\n")
+
+                str = "".join(list(tmp))
+
+                if "Copyright" in str:
+                    continue
+                self.__text.append(str)
+                boolstart = boolend = False
+        result = "".join(list(self.__text))
+        return result
+
+    def replaceCharEntity(self, htmlstr):
+        CHAR_ENTITIES = {'nbsp': ' ', '160': ' ',
+                         'lt': '<', '60': '<',
+                         'gt': '>', '62': '>',
+                         'amp': '&', '38': '&',
+                         'quot': '"', '34': '"', }
+        re_charEntity = re.compile(r'&#?(?P<name>\w+);')
+        sz = re_charEntity.search(htmlstr)
+        while sz:
+            entity = sz.group()
+            key = sz.group('name')
+            try:
+                htmlstr = re_charEntity.sub(CHAR_ENTITIES[key], htmlstr, 1)
+                sz = re_charEntity.search(htmlstr)
+            except KeyError:
+                # 以空串代替
+                htmlstr = re_charEntity.sub('', htmlstr, 1)
+                sz = re_charEntity.search(htmlstr)
+        return htmlstr
+
+    def getHtml(self, url):
+        try:
+            response = requests.get(url)
+        except Exception as e:
+            return None
+
+        if response.status_code == 200:
+            coding = response.encoding
+            page = response.content
+            return page.decode(coding)
+        else:
+            return None
+
+    def readHtml(self, path, coding):
+        page = open(path, encoding=coding)
+        lines = page.readlines()
+        s = ''
+        for line in lines:
+            s += line
+        page.close()
+        return s
+
+    def filter_tags(self, htmlstr):
+        re_nav = re.compile('<nav.+</nav>')
+        re_cdata = re.compile('//<!\[CDATA\[.*//\]\]>', re.DOTALL)
+        re_script = re.compile(
+            '<\s*script[^>]*>.*?<\s*/\s*script\s*>', re.DOTALL | re.I)
+        re_style = re.compile(
+            '<\s*style[^>]*>.*?<\s*/\s*style\s*>', re.DOTALL | re.I)
+        re_textarea = re.compile(
+            '<\s*textarea[^>]*>.*?<\s*/\s*textarea\s*>', re.DOTALL | re.I)
+        re_br = re.compile('<br\s*?/?>')
+        re_h = re.compile('</?\w+.*?>', re.DOTALL)
+        re_comment = re.compile('<!--.*?-->', re.DOTALL)
+        re_space = re.compile(' +')
+        s = re_cdata.sub('', htmlstr)
+        s = re_nav.sub('', s)
+        s = re_script.sub('', s)
+        s = re_style.sub('', s)
+        s = re_textarea.sub('', s)
+        s = re_br.sub('', s)
+        s = re_h.sub('', s)
+        s = re_comment.sub('', s)
+        s = re.sub('\\t', '', s)
+        # s = re.sub(' ', '', s)
+        s = re_space.sub(' ', s)
+        s = self.replaceCharEntity(s)
+        return s
+
+STOP_WORD_LIST = """a,a's,able,about,above,according,accordingly,across,actually,after,afterwards,again,against,ain't,all,allow,allows,almost,alone,along,already,also,although,always,am,among,amongst,an,and,another,any,anybody,anyhow,anyone,anything,anyway,anyways,anywhere,apart,appear,appreciate,appropriate,are,aren't,around,as,aside,ask,asking,associated,at,available,away,awfully,b,be,became,because,become,becomes,becoming,been,before,beforehand,behind,being,believe,below,beside,besides,best,better,between,beyond,both,brief,but,by,c,c'mon,c's,came,can,can't,cannot,cant,cause,causes,certain,certainly,changes,clearly,co,com,come,comes,concerning,consequently,consider,considering,contain,containing,contains,corresponding,could,couldn't,course,currently,d,definitely,described,despite,did,didn't,different,do,does,doesn't,doing,don't,done,down,downwards,during,e,each,edu,eg,eight,either,else,elsewhere,enough,entirely,especially,et,etc,even,ever,every,everybody,everyone,everything,everywhere,ex,exactly,example,except,f,far,few,fifth,first,five,followed,following,follows,for,former,formerly,forth,four,from,further,furthermore,g,get,gets,getting,given,gives,go,goes,going,gone,got,gotten,greetings,h,had,hadn't,happens,hardly,has,hasn't,have,haven't,having,he,he's,hello,help,hence,her,here,here's,hereafter,hereby,herein,hereupon,hers,herself,hi,him,himself,his,hither,hopefully,how,howbeit,however,i,i'd,i'll,i'm,i've,ie,if,ignored,immediate,in,inasmuch,inc,indeed,indicate,indicated,indicates,inner,insofar,instead,into,inward,is,isn't,it,it'd,it'll,it's,its,itself,j,just,k,keep,keeps,kept,know,knows,known,l,last,lately,later,latter,latterly,least,less,lest,let,let's,like,liked,likely,little,look,looking,looks,ltd,m,mainly,many,may,maybe,me,mean,meanwhile,merely,might,more,moreover,most,mostly,much,must,my,myself,n,name,namely,nd,near,nearly,necessary,need,needs,neither,never,nevertheless,new,next,nine,no,nobody,non,none,noone,nor,normally,not,nothing,novel,now,nowhere,o,obviously,of,off,often,oh,ok,okay,old,on,once,one,ones,only,onto,or,other,others,otherwise,ought,our,ours,ourselves,out,outside,over,overall,own,p,particular,particularly,per,perhaps,placed,please,plus,possible,presumably,probably,provides,q,que,quite,qv,r,rather,rd,re,really,reasonably,regarding,regardless,regards,relatively,respectively,right,s,said,same,saw,say,saying,says,second,secondly,see,seeing,seem,seemed,seeming,seems,seen,self,selves,sensible,sent,serious,seriously,seven,several,shall,she,should,shouldn't,since,six,so,some,somebody,somehow,someone,something,sometime,sometimes,somewhat,somewhere,soon,sorry,specified,specify,specifying,still,sub,such,sup,sure,t,t's,take,taken,tell,tends,th,than,thank,thanks,thanx,that,that's,thats,the,their,theirs,them,themselves,then,thence,there,there's,thereafter,thereby,therefore,therein,theres,thereupon,these,they,they'd,they'll,they're,they've,think,third,this,thorough,thoroughly,those,though,three,through,throughout,thru,thus,to,together,too,took,toward,towards,tried,tries,truly,try,trying,twice,two,u,un,under,unfortunately,unless,unlikely,until,unto,up,upon,us,use,used,useful,uses,using,usually,uucp,v,value,various,very,via,viz,vs,w,want,wants,was,wasn't,way,we,we'd,we'll,we're,we've,welcome,well,went,were,weren't,what,what's,whatever,when,whence,whenever,where,where's,whereafter,whereas,whereby,wherein,whereupon,wherever,whether,which,while,whither,who,who's,whoever,whole,whom,whose,why,will,willing,wish,with,within,without,won't,wonder,would,would,wouldn't,x,y,yes,yet,you,you'd,you'll,you're,you've,your,yours,yourself,yourselves,z,zero,"""
+
+STOP_WORD_LIST = STOP_WORD_LIST.split(',')
+
+def is_number(s):
+    try:
+        float(s) if '.' in s else int(s)
+        return True
+    except ValueError:
+        return False
+
+
+def load_stop_words():
+    """
+    Utility function to load stop words from a file and return as a list of words
+    @param stop_word_file Path and file name of a file containing stop words.
+    @return list A list of stop words.
+    """
+    return STOP_WORD_LIST
+
+
+def separate_words(text, min_word_return_size):
+    """
+    Utility function to return a list of all words that are have a length greater than a specified number of characters.
+    @param text The text that must be split in to words.
+    @param min_word_return_size The minimum no of characters a word must have to be included.
+    """
+    splitter = re.compile('[^a-zA-Z0-9_\\+\\-/]')
+    words = []
+    for single_word in splitter.split(text):
+        current_word = single_word.strip().lower()
+        # leave numbers in phrase, but don't count as words, since they tend to invalidate scores of their phrases
+        if len(current_word) > min_word_return_size and current_word != '' and not is_number(current_word):
+            words.append(current_word)
+    return words
+
+
+def split_sentences(text):
+    """
+    Utility function to return a list of sentences.
+    @param text The text that must be split in to sentences.
+    """
+    sentence_delimiters = re.compile(u'[\\[\\]\n.!?,;:\t\\-\\"\\(\\)\\\'\u2019\u2013]')
+    sentences = sentence_delimiters.split(text)
+    return sentences
+
+
+def build_stop_word_regex(stop_word_list):
+    stop_word_regex_list = []
+    for word in stop_word_list:
+        word_regex = '\\b' + word + '\\b'
+        stop_word_regex_list.append(word_regex)
+    stop_word_pattern = re.compile('|'.join(stop_word_regex_list), re.IGNORECASE)
+    return stop_word_pattern
+
+
+#
+# Function that extracts the adjoined candidates from a list of sentences and filters them by frequency
+#
+def extract_adjoined_candidates(sentence_list, stoplist, min_keywords, max_keywords, min_freq):
+    adjoined_candidates = []
+    for s in sentence_list:
+        # Extracts the candidates from each single sentence and adds them to the list
+        adjoined_candidates += adjoined_candidates_from_sentence(s, stoplist, min_keywords, max_keywords)
+    # Filters the candidates and returns them
+    return filter_adjoined_candidates(adjoined_candidates, min_freq)
+
+
+# return adjoined_candidates
+
+#
+# Function that extracts the adjoined candidates from a single sentence
+#
+def adjoined_candidates_from_sentence(s, stoplist, min_keywords, max_keywords):
+    # Initializes the candidate list to empty
+    candidates = []
+    # Splits the sentence to get a list of lowercase words
+    sl = s.lower().split()
+    # For each possible length of the adjoined candidate
+    for num_keywords in range(min_keywords, max_keywords + 1):
+        # Until the third-last word
+        for i in range(0, len(sl) - num_keywords):
+            # Position i marks the first word of the candidate. Proceeds only if it's not a stopword
+            if sl[i] not in stoplist:
+                candidate = sl[i]
+                # Initializes j (the pointer to the next word) to 1
+                j = 1
+                # Initializes the word counter. This counts the non-stopwords words in the candidate
+                keyword_counter = 1
+                contains_stopword = False
+                # Until the word count reaches the maximum number of keywords or the end is reached
+                while keyword_counter < num_keywords and i + j < len(sl):
+                    # Adds the next word to the candidate
+                    candidate = candidate + ' ' + sl[i + j]
+                    # If it's not a stopword, increase the word counter. If it is, turn on the flag
+                    if sl[i + j] not in stoplist:
+                        keyword_counter += 1
+                    else:
+                        contains_stopword = True
+                    # Next position
+                    j += 1
+                # Adds the candidate to the list only if:
+                # 1) it contains at least a stopword (if it doesn't it's already been considered)
+                # AND
+                # 2) the last word is not a stopword
+                # AND
+                # 3) the adjoined candidate keyphrase contains exactly the correct number of keywords (to avoid doubles)
+                if contains_stopword and candidate.split()[-1] not in stoplist and keyword_counter == num_keywords:
+                    candidates.append(candidate)
+    return candidates
+
+#
+# Function that filters the adjoined candidates to keep only those that appears with a certain frequency
+#
+def filter_adjoined_candidates(candidates, min_freq):
+    # Creates a dictionary where the key is the candidate and the value is the frequency of the candidate
+    candidates_freq = Counter(candidates)
+    filtered_candidates = []
+    # Uses the dictionary to filter the candidates
+    for candidate in candidates:
+        freq = candidates_freq[candidate]
+        if freq >= min_freq:
+            filtered_candidates.append(candidate)
+    return filtered_candidates
+
+
+def generate_candidate_keywords(sentence_list, stopword_pattern, stop_word_list, min_char_length=1, max_words_length=5,
+                                min_words_length_adj=1, max_words_length_adj=1, min_phrase_freq_adj=2):
+    phrase_list = []
+    for s in sentence_list:
+        tmp = re.sub(stopword_pattern, '|', s.strip())
+        phrases = tmp.split("|")
+        for phrase in phrases:
+            phrase = phrase.strip().lower()
+            if phrase != "" and is_acceptable(phrase, min_char_length, max_words_length):
+                phrase_list.append(phrase)
+    phrase_list += extract_adjoined_candidates(sentence_list, stop_word_list, min_words_length_adj,
+                                               max_words_length_adj, min_phrase_freq_adj)
+    return phrase_list
+
+
+def is_acceptable(phrase, min_char_length, max_words_length):
+    # a phrase must have a min length in characters
+    if len(phrase) < min_char_length:
+        return 0
+
+    # a phrase must have a max number of words
+    words = phrase.split()
+    if len(words) > max_words_length:
+        return 0
+
+    digits = 0
+    alpha = 0
+    for i in range(0, len(phrase)):
+        if phrase[i].isdigit():
+            digits += 1
+        elif phrase[i].isalpha():
+            alpha += 1
+
+    # a phrase must have at least one alpha character
+    if alpha == 0:
+        return 0
+
+    # a phrase must have more alpha than digits characters
+    if digits > alpha:
+        return 0
+    return 1
+
+
+def calculate_word_scores(phraseList):
+    word_frequency = {}
+    word_degree = {}
+    for phrase in phraseList:
+        word_list = separate_words(phrase, 0)
+        word_list_length = len(word_list)
+        word_list_degree = word_list_length - 1
+        # if word_list_degree > 3: word_list_degree = 3 #exp.
+        for word in word_list:
+            word_frequency.setdefault(word, 0)
+            word_frequency[word] += 1
+            word_degree.setdefault(word, 0)
+            word_degree[word] += word_list_degree  # orig.
+            # word_degree[word] += 1/(word_list_length*1.0) #exp.
+    for item in word_frequency:
+        word_degree[item] = word_degree[item] + word_frequency[item]
+
+    # Calculate Word scores = deg(w)/frew(w)
+    word_score = {}
+    for item in word_frequency:
+        word_score.setdefault(item, 0)
+        word_score[item] = word_degree[item] / (word_frequency[item] * 1.0)  # orig.
+    # word_score[item] = word_frequency[item]/(word_degree[item] * 1.0) #exp.
+    return word_score
+
+
+def generate_candidate_keyword_scores(phrase_list, word_score, min_keyword_frequency=1):
+    keyword_candidates = {}
+    for phrase in phrase_list:
+        if min_keyword_frequency > 1:
+            if phrase_list.count(phrase) < min_keyword_frequency:
+                continue
+        keyword_candidates.setdefault(phrase, 0)
+        word_list = separate_words(phrase, 0)
+        candidate_score = 0
+        for word in word_list:
+            candidate_score += word_score[word]
+        keyword_candidates[phrase] = candidate_score
+    return keyword_candidates
+
+
+class Rake(object):
+    def __init__(self, min_char_length=1, max_words_length=5, min_keyword_frequency=1,
+                 min_words_length_adj=1, max_words_length_adj=1, min_phrase_freq_adj=2):
+        self.__stop_words_list = load_stop_words()
+        self.__min_char_length = min_char_length
+        self.__max_words_length = max_words_length
+        self.__min_keyword_frequency = min_keyword_frequency
+        self.__min_words_length_adj = min_words_length_adj
+        self.__max_words_length_adj = max_words_length_adj
+        self.__min_phrase_freq_adj = min_phrase_freq_adj
+
+    def run(self, text):
+        sentence_list = split_sentences(text)
+
+        stop_words_pattern = build_stop_word_regex(self.__stop_words_list)
+
+        phrase_list = generate_candidate_keywords(sentence_list, stop_words_pattern, self.__stop_words_list,
+                                                  self.__min_char_length, self.__max_words_length,
+                                                  self.__min_words_length_adj, self.__max_words_length_adj,
+                                                  self.__min_phrase_freq_adj)
+
+        word_scores = calculate_word_scores(phrase_list)
+
+        keyword_candidates = generate_candidate_keyword_scores(phrase_list, word_scores, self.__min_keyword_frequency)
+
+        sorted_keywords = sorted(six.iteritems(keyword_candidates), key=operator.itemgetter(1), reverse=True)
+        return sorted_keywords
+
 
 def read_input(file):
     for line in file:
@@ -17,7 +386,7 @@ def read_input(file):
 
 def main(separator=','):
     cx = extractor()
-    rake_object = rake.Rake(conf.STOPWORD, 2, 3, 1)
+    rake_object = rake.Rake(2, 3, 1)
 
     # input comes from STDIN (standard input)
     url_list = read_input(sys.stdin)
