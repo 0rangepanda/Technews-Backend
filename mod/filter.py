2f8ef59de65f0a14d90dbebef07fd07033cb1684
Roger <jayluo9410@gmail.com>
2017-11-19 12:56:40 -0800

    Newest

--- mod/filter.py
+++ mod/filter.py
@@ -14,11 +14,11 @@ def cal_score(result_path, tech, topK=10):
             tech_list[line[:-1]] = 0
 
     # TODO: read all the file under result_path
-    files= os.listdir(result_path) #得到文件夹下的所有文件名称  
-    for file in files: #遍历文件夹
-         if not os.path.isdir(file): #判断是否是文件夹，不是文件夹才打开
-              f = open(path+"/"+file); #打开文件
-              iter_f = iter(f); #创建迭代器
+    files= os.listdir(result_path)
+    for file in files:
+         if not os.path.isdir(file):
+              f = open(path+"/"+file);
+              iter_f = iter(f);
               for line in f:
                   word,score = line.rstrip().split(',', 1)
                   for tech_word in tech_list:
